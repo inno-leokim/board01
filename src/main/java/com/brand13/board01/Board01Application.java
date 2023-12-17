323e451a666daca92a01dc48e9797bc02cb30062
inno-leokim <inno.leokim@gmail.com>
2023-12-18 01:16:06 +0900

    Edit devtools restart from true to false

--- src/main/java/com/brand13/board01/Board01Application.java
+++ src/main/java/com/brand13/board01/Board01Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class Board01Application {
 
 	public static void main(String[] args) {
+		// System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(Board01Application.class, args);
 	}
 
